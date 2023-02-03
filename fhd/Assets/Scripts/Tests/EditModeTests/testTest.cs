7054a1f1cd8555df1e78d3d289f25d79affb3d47
Georg Becker <georg.becker@shellrider.com>
2023-02-03 21:32:39 +0100

    Setup Tests

---
+++ fhd/Assets/Scripts/Tests/EditModeTests/testTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class testTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void testTestSimplePasses()
+    {
+        Assert.Pass();
+    }
+    
+}
