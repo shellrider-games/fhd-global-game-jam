80d37b4f0cf1ad52df240b9934a7ffc0f0620769
Georg Becker <georg.becker@shellrider.com>
2023-02-03 23:39:07 +0100

    Implements Collectible

--- fhd/Assets/Scripts/Tests/PlayModeTests/testCollectible.cs
+++ fhd/Assets/Scripts/Tests/PlayModeTests/testCollectible.cs
@@ -6,12 +6,30 @@ using UnityEngine.TestTools;
 
 public class testCollectible
 {
+    private GameObject _gameObject;
+    private Collectible _collectible;
+    private CollectibleState _collectibleState;
+    [SetUp]
+    public void SetUp()
+    {
+        _gameObject = new GameObject();
+        _collectible = _gameObject.AddComponent(typeof(Collectible)) as Collectible;
+        _collectibleState = _gameObject.AddComponent(typeof(CollectibleState)) as CollectibleState;
+    }
+    
     [UnityTest]
     public IEnumerator testCollectibleCanExist() {
-        GameObject collectible = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Collectible"));
-        Assert.NotNull(collectible);
+        Assert.NotNull(_collectible);
         yield return null;
     }
 
+    [UnityTest]
+    public IEnumerator testCollectibleIncreasesCollectedStateWhenCollected()
+    {
+        Assert.That(CollectibleState.Instance.collectablesCollected, Is.EqualTo(0));
+        _collectible.Collect();
+        Assert.That(CollectibleState.Instance.collectablesCollected, Is.EqualTo(1));
+        yield return null;
+    }
 }
 
